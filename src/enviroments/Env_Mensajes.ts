0dec9a6ab12b500093dca6e83d2e675b7d10422d
Jeanfranco31 <jfintriagoineval@gmail.com>
2024-07-03 13:39:54 -0500

    update

--- src/enviroments/Env_Mensajes.ts
+++ src/enviroments/Env_Mensajes.ts
@@ -1,6 +1,7 @@
 export const Env_Mensajes = {
     error: "Error",
     exito: "Exito",
+    stringVacio:'',
     errorObtenerDatos : "Error al obtener datos",
     usuarioNoSeleccionado : "Debes seleccionar un usuario para eliminarlo",
     usuarioDesbloqueado: "Usuario Desbloqueado",
